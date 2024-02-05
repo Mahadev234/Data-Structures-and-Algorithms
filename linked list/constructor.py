64093149e4f6d84526bdb310972125d1807c59db
Mahadev234 <mtelidev@gitam.in>
2024-02-05 08:27:20 +0530

    Initial Commit

---
+++ linked list/constructor.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,value):
+        pass
+class LinkedList:
+    # Create a new node
+    def __init__(self, value):
+        pass
+    # create a node and add it to the end
+    def append(self,value):
+        pass
+    # create a node and add it to the beginning
+    def prepend(self,value):
+        pass
+    # create a node and add it to the given index
+    def insert(self,index,value):
+        pass
